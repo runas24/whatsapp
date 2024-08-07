4d8ecd7d81d3685cf2aa85ef1bd320299768f93a
runas24 <runnnasss4@gmail.com>
2024-08-07 15:56:07 +0500

    cvxcvxc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src', // Укажите папку, где находится ваш index.html
+  root: 'src',
   build: {
-    outDir: '../dist' // Укажите папку, куда должны быть собраны файлы
+    outDir: '../dist'
   }
 });
