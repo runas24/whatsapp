d637acf8e225e6e34452f996dac0144355156b67
runas24 <runnnasss4@gmail.com>
2024-08-07 15:43:06 +0500

    РПР

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src', // Укажите папку, где находится ваш index.html
   build: {
-    outDir: 'dist'
+    outDir: '../dist' // Укажите папку, куда должны быть собраны файлы
   }
 });
